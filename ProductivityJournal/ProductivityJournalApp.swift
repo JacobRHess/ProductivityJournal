f0a49eccd82e33e09bc9b3bf7328d0ba5a00d107
Jacob H <hess.jacobr@gmail.com>
2025-07-16 16:04:15 -0500

    Initial Commit

---
+++ ProductivityJournal/ProductivityJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductivityJournalApp.swift
+//  ProductivityJournal
+//
+//  Created by Jacob Hess on 7/16/25.
+//
+
+import SwiftUI
+
+@main
+struct ProductivityJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProductivityJournalTests/ProductivityJournalTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProductivityJournalTests.swift
+//  ProductivityJournalTests
+//
+//  Created by Jacob Hess on 7/16/25.
+//
+
+import Testing
+@testable import ProductivityJournal
+
+struct ProductivityJournalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
